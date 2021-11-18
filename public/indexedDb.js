78a65a8e4b0c89e85ce7b2e776de43d38691a4df
unknown <filip.a.11@hotmail.com>
2021-11-17 22:23:36 -0500

    added mongodb uri, opened indexeddb, instantiated it, created object store

---
+++ public/indexedDb.js
@@ -0,0 +1,47 @@
+let db;
+let version;
+
+const request = indexedDb.open('BudgetDb', version || 1)
+
+request.onupgradeneeded = function(event) {
+    const { previousVersion } = event
+    const newVersion = event.newVersion || db.version
+
+    db = event.target.result
+
+    if(db.objectStoreNames.length === 0) {
+        db.objectStoreNames('BudgetStore', { autoIncrement : true })
+    }
+}
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode)
+}
+
+function CheckDatabase() {
+
+    let transaction = db.transaction(['BudgetStore'], 'readwrite')
+
+    const store = transaction.objectStore('BudgetStore')
+
+    const getAll = store.getAll()
+
+    getAll.onsucess = function() {
+        if(getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(getAll.result),
+            })
+                .then((response) => response.json())
+                    .then((data) => {
+                        if (data.length !== 0) {
+                            transaction = db.transaction(['BudgetStore'], 'readwrite')
+                        }
+                    })
+        }
+    }
+}
