2a451e8e8f4cbf479607842c20a3b21d54857a12
unknown <filip.a.11@hotmail.com>
2021-11-17 23:44:21 -0500

    heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require('dotenv').config()
 
 const PORT = 3000;
 
@@ -15,10 +16,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://FilipAlH:Password123@cluster0.innr5.mongodb.net/budget?retryWrites=true&w=majority", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://FilipAlH:Password123@cluster0.innr5.mongodb.net/budget?retryWrites=true&w=majority", 
+{
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 // routes
